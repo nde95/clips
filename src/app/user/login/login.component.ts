f852666a9aba4259e4103aa6ba145d2cb2437c3f
nde95 <ndennison268@gmail.com>
2023-05-10 17:18:42 -0400

    added login verification

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FirebaseError } from '@angular/fire/app';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +13,40 @@ export class LoginComponent {
     password: '',
   }
 
+  showAlert = false 
+  alertMsg = 'Please wait, we are logging you in!'
+  alertColor = 'blue'
+  inSubmission = false 
+
   public emailPattern = '[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?';
 
+  constructor(private auth: AngularFireAuth) {  }
 
+  async login() {
+    this.showAlert = true
+    this.alertMsg = "Logging in, please wait..."
+    this.alertColor = "blue"
+    this.inSubmission = true
+    try {
+      await this.auth.signInWithEmailAndPassword(this.credentials.email, this.credentials.password)
+        this.alertMsg = 'Login successful!';
+        this.alertColor = 'green';
+        this.inSubmission = true
+    } catch(e) {
+      console.log(e);
 
-  login() {
-    console.log(this.credentials)
+      if (e instanceof FirebaseError) {
+        const firebaseError: FirebaseError = e;
+
+        if (firebaseError.code === 'auth/user-not-found') {
+          this.alertMsg = 'Invalid user, please check your login information and try again. ';
+          this.inSubmission = false;
+        } else {
+          this.alertMsg = 'Error logging in. Please try again later.';
+          this.inSubmission = false;
+        }
+      }
+      this.alertColor = 'red';
+    }
   }
 }
