1a67766e57813dbdafbe5fffde6e4ca3bb057d6e
nde95 <ndennison268@gmail.com>
2023-05-10 13:06:21 -0400

    added firebase authentication to register function

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FirebaseError } from '@angular/fire/app';
+
 
 @Component({
   selector: 'app-register',
@@ -7,6 +10,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(private auth: AngularFireAuth) {}
+
+  inSubmission = false
+
   name = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
@@ -47,9 +54,34 @@ export class RegisterComponent {
    phoneNumber: this.phoneNumber,
   })
 
-  register() {
+  async register() {
     this.showAlert = true
     this.alertMsg = "Account is being created, please wait!"
     this.alertColor = "blue"
+    this.inSubmission = true
+
+    const { email, password } = this.registerForm.value;
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword(email as string, password as string);
+      console.log(userCred);
+      this.alertMsg = 'Success, your account has been created!';
+      this.alertColor = 'green';
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof FirebaseError) {
+        const firebaseError: FirebaseError = error;
+
+        if (firebaseError.code === 'auth/email-already-in-use') {
+          this.alertMsg = 'The email address is already in use. Please use a different email.';
+        } else {
+          this.alertMsg = 'An unexpected Firebase error has occurred. Please try again later.';
+        }
+      } else {
+        this.alertMsg = 'An unexpected error has occurred. Please try again later.';
+      }
+
+      this.alertColor = 'red';
+    }
   }
 }
