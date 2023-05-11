225bc0133f42ed51585b1225c8456e4da857717b
nde95 <ndennison268@gmail.com>
2023-05-11 19:15:08 -0400

    adding confirm password check

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { FirebaseError } from '@angular/fire/app';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+
 
 @Component({
   selector: 'app-register',
@@ -54,7 +56,7 @@ export class RegisterComponent {
    password: this.password,
    confirm_password: this.confirm_password,
    phoneNumber: this.phoneNumber,
-  })
+  }, [RegisterValidators.match('password', 'confirm_password')])
 
   async register() {
     this.showAlert = true
