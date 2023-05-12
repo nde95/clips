f2d56efc8e8b1755626c7865b6e322401ecad65a
nde95 <ndennison268@gmail.com>
2023-05-12 14:02:50 -0400

    added upload route/page for auth users

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
+import { UploadComponent } from './upload/upload.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,13 @@ const routes: Routes = [
       authOnly: true
     }
   },
+  {
+    path: 'upload',
+    component: UploadComponent,
+    data: {
+      authOnly:true
+    }
+  }
 ];
 
 @NgModule({
