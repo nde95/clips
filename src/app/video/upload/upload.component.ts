39674ec28f232cf0fd74657b0c1fe12dd72c151d
nde95 <ndennison268@gmail.com>
2023-05-13 10:26:22 -0400

    added conditionals to hide video editor until use

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -6,14 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
+  fileUploaded = false
   isDragOver = false
+  file: File | null = null
 
   constructor() {
 
   }
 
   storeFile($event: Event) {
+   
     this.isDragOver = false
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null 
+
+    if(!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+    this.fileUploaded = true;
+    console.log(this.file)
   }
 
 }
