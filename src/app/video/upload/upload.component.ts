1419cf00d92101ae70a51114213791411f6b64aa
nde95 <ndennison268@gmail.com>
2023-05-13 12:03:51 -0400

    handling successful/unsuccesful uploads with alert

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage'
-import { v4 as uuid } from 'uuid'
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { last } from 'rxjs/operators';
 
 
 @Component({
@@ -13,6 +15,12 @@ export class UploadComponent {
   fileUploaded = false
   isDragOver = false
   file: File | null = null
+  showAlert = false
+  alertMsg = 'Uploading clip, please wait...'
+  alertColor = 'blue'
+  inSubmission = false 
+  percentage = 0
+  showPercentage = false
 
   title = new FormControl('',{
     validators: [
@@ -45,10 +53,39 @@ export class UploadComponent {
   }
 
   uploadFile() {
-    const clipFileName = uuid()
-    const clipPath = `clips/${clipFileName}.mp4`
-
-    this.storage.upload(clipPath, this.file)
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Uploading clip, please wait...';
+    this.inSubmission = true;
+    this.showPercentage = true;
+  
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+  
+    const task = this.storage.upload(clipPath, this.file);
+  
+    task.percentageChanges().subscribe(progress => {
+      this.percentage = progress as number / 100;
+    });
+  
+    task.snapshotChanges().pipe(last()).subscribe({
+      next: (snapshot) => {
+        this.alertColor = 'green';
+        this.alertMsg = 'Success! Your clip has been uploaded and is ready to share!';
+        this.showPercentage = false;
+      },
+      error: (error) => {
+        this.alertColor = 'red';
+        this.inSubmission = false;
+        this.showPercentage = false;
+        
+        if (error.code === 'storage/unauthorized') {
+          this.alertMsg = 'File too big! Maximum video size is 25MB.';
+        } else {
+          this.alertMsg = 'Upload failed, please try again later.';
+          console.error(error);
+        }
+      }
+    });
   }
-
 }
