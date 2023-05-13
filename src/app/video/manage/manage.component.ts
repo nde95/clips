ea023ff664050f84dfd619616ecdb12aab819ea6
nde95 <ndennison268@gmail.com>
2023-05-13 18:30:10 -0400

    allow sorting by timestamp of upload in manage

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -3,6 +3,8 @@ import { Router, ActivatedRoute, Params } from '@angular/router'
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
 import { ModalService } from 'src/app/services/modal.service';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-manage',
@@ -11,22 +13,26 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 
 export class ManageComponent {
-  videoOrder = ''
+  videoOrder = '1'
   clips: IClip[] = []
   activeClip: IClip | null = null
+  sort$: BehaviorSubject<string>
   
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private clipService: ClipService,
     private modal: ModalService,
-    ) {}
+    ) {
+      this.sort$ = new BehaviorSubject(this.videoOrder)
+    }
 
     ngOnInit(): void {
       this.route.queryParams.subscribe((params: Params) => {
         this.videoOrder = params.sort === '2' ? params.sort : '1'
+        this.sort$.next(this.videoOrder)
       })
-      this.clipService.getUserClips().subscribe(docs => {
+      this.clipService.getUserClips(this.sort$).subscribe(docs => {
         this.clips = []
 
         docs.forEach(doc => {
