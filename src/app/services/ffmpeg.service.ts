be7f92449020c7bbb35ec6f3b3daf67e31c72a10
nde95 <ndennison268@gmail.com>
2023-05-14 13:33:12 -0400

    rendering screenshots from video frames w/ ffmpeg

---
+++ src/app/services/ffmpeg.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FfmpegService {
+  isReady = false 
+  private ffmpeg
+
+  constructor() { 
+    this.ffmpeg = createFFmpeg({ log: true })
+   }
+
+   async init() {
+    if(this.isReady) {
+      return
+    }
+
+    await this.ffmpeg.load() 
+
+    this.isReady = true
+   }
+
+   async getScreenshots(file: File) {
+    const data = await fetchFile(file)
+
+    this.ffmpeg.FS('writeFile', file.name, data)
+
+    const seconds = [1,3,5]
+    const commands: string[] = []
+
+    seconds.forEach(second => {
+      commands.push(
+        //input
+      '-i', file.name,
+      //output options
+      '-ss', `00:00:0${second}`,
+      '-frames:v', '1',
+      '-filter:v', 'scale=510:-1',
+      //output
+      `output_0${second}.png`
+      )
+    })
+
+
+
+    await this.ffmpeg.run(
+      ...commands
+    )
+   }
+}
