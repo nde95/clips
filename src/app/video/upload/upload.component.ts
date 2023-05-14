15626aea9151bc4f1a190844ef24834e77bc0faf
nde95 <ndennison268@gmail.com>
2023-05-14 14:15:32 -0400

    refatoring upload prog for screenshots

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -8,7 +8,7 @@ import firebase from 'firebase/compat/app';
 import { ClipService } from 'src/app/services/clip.service';
 import { Router } from '@angular/router'
 import { FfmpegService } from 'src/app/services/ffmpeg.service';
-
+import { combineLatest } from 'rxjs'
 
 @Component({
   selector: 'app-upload',
@@ -105,8 +105,19 @@ export class UploadComponent implements OnDestroy {
     this.screenshotTask = this.storage.upload(screenshotPath, screenshotBlob)
 
 
-    this.task.percentageChanges().subscribe(progress => {
-      this.percentage = progress as number / 100;
+    combineLatest([
+      this.task.percentageChanges(),
+      this.screenshotTask.percentageChanges()
+    ]).subscribe((progress) => {
+      const [clipProgress, screenshotProgress] = progress
+
+      if(!clipProgress || !screenshotProgress) {
+        return
+      }
+
+      const total = clipProgress + screenshotProgress
+
+      this.percentage = total as number / 200;
     });
   
     this.task.snapshotChanges().pipe(last(), switchMap(() => clipRef.getDownloadURL())).subscribe({
