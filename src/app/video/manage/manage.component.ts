b0d8b452fff7c8f328db4ccfa026a2358578625a
nde95 <ndennison268@gmail.com>
2023-05-15 11:31:13 -0400

    add functionality to copy link to clipboard

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -78,4 +78,18 @@ export class ManageComponent {
     })
   }
 
+  async copyToClipboard($event: MouseEvent, docID: string | undefined) {
+    $event.preventDefault()
+
+    if(!docID) {
+      return
+    }
+
+    const url = `${location.origin}/clip/${docID}`
+
+    await navigator.clipboard.writeText(url)
+
+    alert('Link copied to clipboard!')
+  }
+
 }
