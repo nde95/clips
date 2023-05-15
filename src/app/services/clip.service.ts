3d564c5a2115da8862543ebd7b885c51c9135580
nde95 <ndennison268@gmail.com>
2023-05-14 21:27:40 -0400

    rendering user uploaded clips on homepage

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -11,6 +11,8 @@ import { AngularFireStorage } from '@angular/fire/compat/storage'
 })
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>
+  pageClips: IClip[] = []
+  pendingRequest = false 
 
   constructor(
     private db: AngularFirestore,
@@ -63,5 +65,36 @@ export class ClipService {
     await screenshoRef.delete()
 
     await this.clipsCollection.doc(clip.docID).delete()
+   }
+
+   async getClips() {
+    if(this.pendingRequest) {
+      return 
+    }
+    
+    this.pendingRequest = true 
+    let query = this.clipsCollection.ref.orderBy('timestamp', 'desc').limit(6)
+
+    const { length } = this.pageClips
+
+    if(length) {
+      const lastDocID = this.pageClips[length - 1].docID
+      const lastDoc = await this.clipsCollection.doc(lastDocID)
+      .get()
+      .toPromise()
+
+      query = query.startAfter(lastDoc)
+    }
+
+    const snapshot = await query.get()
+
+    snapshot.forEach(doc => {
+      this.pageClips.push({
+        docID: doc.id,
+        ...doc.data()
+      })
+    })
+
+    this.pendingRequest = false 
    }
 }
